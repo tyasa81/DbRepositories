2398ed3e8cbc889db1de260d61c58770045022a0
Tony Yasa <tyasa81@gmail.com>
2024-09-29 22:55:33 +0700

    delete all unnecessary files

--- src/DbRepositoriesServiceProvider.php
+++ src/DbRepositoriesServiceProvider.php
@@ -7,12 +7,11 @@ use Illuminate\Support\ServiceProvider;
 
 class DbRepositoriesServiceProvider extends ServiceProvider
 {
-    public function register() {}
+    public function register() 
+    {
+    }
 
     public function boot()
     {
-        app()->bind('UserRepository', function () {
-            return new UserRepository;
-        });
     }
 }

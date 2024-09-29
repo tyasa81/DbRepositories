21e0bd7b896d04da24979a1d5fd3ce9f5239f5d1
tyasa81 <tyasa81@users.noreply.github.com>
2024-09-29 15:55:51 +0000

    Fix styling

--- src/DbRepositoriesServiceProvider.php
+++ src/DbRepositoriesServiceProvider.php
@@ -2,16 +2,11 @@
 
 namespace tyasa81\DbRepositories;
 
-use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class DbRepositoriesServiceProvider extends ServiceProvider
 {
-    public function register() 
-    {
-    }
+    public function register() {}
 
-    public function boot()
-    {
-    }
+    public function boot() {}
 }
